c437d0dcddf1c9c8173af4ac41127c4d05cd7879
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-14 01:50:07 +0300

    finish task 6

--- HM1/task6.py
+++ HM1/task6.py
@@ -2,7 +2,9 @@
 
 num = int(input('Input number to check: '))
 
-if num % 5 == 0 and num % 10 == 0 or num % 15 == 0:
-    print('True')
 if num % 30 == 0:
+    print('False')
+elif num % 5 == 0 or num % 10 == 0 or num % 15 == 0:
+    print('True')
+else:
     print('False')
