89989e8136f4a02927e494ed53c3bb1c7ec04e18
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-14 02:24:34 +0300

    finish task 4

--- HM1/task4.py
+++ HM1/task4.py
@@ -1,1 +1,12 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+num = int(input('Input quarter number: '))
+
+if num == 1:
+    print('x > 0 and y > 0')
+elif num == 2:
+    print('x < 0 and y > 0')
+elif num == 3:
+    print('x < 0 and y < 0')
+elif num == 4:
+    print('x > 0 and y < 0')
