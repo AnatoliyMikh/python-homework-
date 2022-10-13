7ce5288ea3315b0d0d3fabcdc61727d2a5dd127f
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-14 02:59:26 +0300

    finish task 5

--- HM1/task5.py
+++ HM1/task5.py
@@ -4,3 +4,18 @@
 
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
+
+import math
+
+
+xa = int(input('Input x(A): '))
+
+ya = int(input('Input y(A): '))
+
+xb = int(input('Input x(B): '))
+
+yb = int(input('Input y(B): '))
+
+result = float(math.sqrt((xb-xa)**2+(yb-ya)**2))
+
+print (f'{str(result)[:4]}')
