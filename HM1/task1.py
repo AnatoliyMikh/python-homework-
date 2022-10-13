a8f1df8418b53d9545197221e6cf5b94e33e6c23
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-14 01:57:36 +0300

    finish task 1

--- HM1/task1.py
+++ HM1/task1.py
@@ -5,3 +5,12 @@
 # - 6 -> да
 # - 7 -> да
 # - 1 -> нет
+
+result = int(input('Input number weekday: '))
+
+if result == 6 or result == 7:
+    print('true')
+elif result == 1 or result == 2 or result == 3 or result == 4 or result == 5:
+    print('False')
+else:
+    print('False. Only 7 days in a week')
