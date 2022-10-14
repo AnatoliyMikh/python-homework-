78641bab5dfc1e6b7d75d59e0f1084f34155128f
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-14 13:36:50 +0300

    finish nask 2

--- HM1/task2.py
+++ HM1/task2.py
@@ -1,1 +1,13 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = int(input('Input x: '))
+y = int(input('Input y: '))
+z = int(input('Input z: '))
+
+first = not (x or y or z)
+second = not x and not y and not z
+
+if first == second:
+    print('true')
+else:
+    print('false')
