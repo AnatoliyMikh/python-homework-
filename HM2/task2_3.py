b31cdd39a1c6d5b90dae5073a2893407d0e308ee
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-18 18:08:57 +0300

    finish task 3

--- HM2/task2_3.py
+++ HM2/task2_3.py
@@ -3,3 +3,16 @@
 # Пример:
 
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+
+a = list()
+n = int(input('Input N: '))
+
+sum = 0
+i = 1
+
+while i<=n:
+    a.append((1 + 1 / i)**i)
+    i+=1
+    sum = sum + a[-1] 
+
+print('sum of numbers = ',sum)
