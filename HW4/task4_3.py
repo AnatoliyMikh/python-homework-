7364396f7a1373809d513e15a5d70638d3437679
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-22 21:55:17 +0300

    finish task 3

--- HW4/task4_3.py
+++ HW4/task4_3.py
@@ -1,1 +1,12 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+list1 = input('input subsequence: ')
+list2 = list()
+
+i = 0
+d = len(list1)-1
+for i in range (i, d):
+    if list1.count(list1[i]) == 1:
+            list2.append(list1[i])
+
+print('input subsequence:',list1,';','not repeating elements: ',list2) 
