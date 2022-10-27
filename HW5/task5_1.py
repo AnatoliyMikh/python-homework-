1d04c90695045956c1f321eb166b0ffd015962e2
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-27 21:47:25 +0300

    first result

--- HW5/task5_1.py
+++ HW5/task5_1.py
@@ -1,11 +1,13 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-sample_text = input('Input sample text: ')
-
-print('Original text:', sample_text)
-
-print(edit_text(sample_text))
-
 def edit_text(text):
     t = text.split(" ")
-    
+    new_text = []
+    for i in range (len(t)):
+        if (lambda t:('а' or 'б' or 'в') in t[i]) == False:
+            new_text += t[i]
+    return new_text
+
+sample_text = input('Input sample text: ')
+print('Original text:', sample_text)
+print(edit_text(sample_text))
